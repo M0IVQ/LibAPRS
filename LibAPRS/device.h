930ba77358852cc1d56e60f42d8c79ca87f6bd8a
M0IVQ <post2simonsmith@gmail.com>
2016-06-02 14:10:05 +0100

    Update device.h
    Added ATmega2560 for Arduino Mega 2560 (Hasn't been tested yet so may require other changes)

--- LibAPRS/device.h
+++ LibAPRS/device.h
@@ -19,7 +19,7 @@
 // Sampling & timer setup
 #define CONFIG_AFSK_DAC_SAMPLERATE 9600
 
-// Port settings
+// Port settings ATmega328p
 #if TARGET_CPU == m328p
     #define DAC_PORT PORTD
     #define DAC_DDR  DDRD
@@ -27,6 +27,16 @@
     #define LED_DDR  DDRB
     #define ADC_PORT PORTC
     #define ADC_DDR  DDRC
+#endif
+
+// Port settings ATmega2560 - TX-pin 48, RX-pin 47, PTT-pin 25, DAC-pins 26-29, ADC-pin A0
+#if TARGET_CPU == m2560
+    #define DAC_PORT PORTA
+    #define DAC_DDR  DDRA
+    #define LED_PORT PORTL
+    #define LED_DDR  DDRL
+    #define ADC_PORT PORTF
+    #define ADC_DDR  DDRF
 #endif
 
 #endif
