c5c7448bfecb9e905882627c98c4e7aacce0cb34
M0IVQ <post2simonsmith@gmail.com>
2017-03-08 16:42:08 +0000

    Added pinout comments for 2560

--- LibAPRS/device.h
+++ LibAPRS/device.h
@@ -21,21 +21,21 @@
 
 // Port settings ATmega328p
 #if TARGET_CPU == m328p
-    #define DAC_PORT PORTD
+    #define DAC_PORT PORTD	//D4,D5,D6,D7 (8k2,3k9,2k2,1k), PTT D3
     #define DAC_DDR  DDRD
-    #define LED_PORT PORTB
+    #define LED_PORT PORTB	//TX B1, RX B2
     #define LED_DDR  DDRB
-    #define ADC_PORT PORTC
+    #define ADC_PORT PORTC	//C0
     #define ADC_DDR  DDRC
 #endif
 
 // Port settings ATmega2560 - TX-pin 48, RX-pin 47, PTT-pin 25, DAC-pins 26-29, ADC-pin A0
 #if TARGET_CPU == m2560
-    #define DAC_PORT PORTA
+    #define DAC_PORT PORTA	//A4,A5,A6,A7 (8k2,3k9,2k2,1k), PTT A3
     #define DAC_DDR  DDRA
-    #define LED_PORT PORTL
+    #define LED_PORT PORTL	//TX L1, RX L2
     #define LED_DDR  DDRL
-    #define ADC_PORT PORTF
+    #define ADC_PORT PORTF	//F0
     #define ADC_DDR  DDRF
 #endif
 
